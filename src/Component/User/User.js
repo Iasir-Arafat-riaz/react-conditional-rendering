a1dd942f0e409b36d5f993cf022672fd9afabc96
Iasir-Arafat-Riaz <iasir.arafat.riaz@gmail.com>
2021-05-31 19:34:20 +0600

    new commit

--- src/Component/User/User.js
+++ src/Component/User/User.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
 const User = (props) => {
-    const familiar =
+    const familiar = props.familiar
+    let greetings;
+    if (familiar===true){
+        greetings = <p>Welcome My Friend</p>
+    }
+    else{
+        greetings = <p>Who the hell are you ?</p>
+    }
     return (
         <div>
-            
+            {greetings}
         </div>
     );
 };
