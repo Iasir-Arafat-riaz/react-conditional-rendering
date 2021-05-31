06574aaa321f8714fb1a81bbce8031e2c09f30e3
Iasir-Arafat-Riaz <iasir.arafat.riaz@gmail.com>
2021-05-31 18:48:07 +0600

    new commit

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import { useState } from 'react';
 
 function App() {
   const [familiar, setFamiliar]=useState(false)
+  const functio=()=>setFamiliar(!familiar)
   return (
     <div className="App">
       <h2>Is Familiar : {familiar.toString()}</h2>
-      <button onClick={()=>setFamiliar(!familiar)}>Toggle Friend</button>
+      <button onClick={functio}>Toggle Friend</button>
   
     
     </div>
