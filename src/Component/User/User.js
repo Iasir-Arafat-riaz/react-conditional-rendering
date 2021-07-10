3ffbdfb88bd303e72d05ad80b5e9d27e69b8f41c
Iasir-Arafat-riaz <76946854+Iasir-Arafat-riaz@users.noreply.github.com>
2021-07-10 23:53:07 +0600

    git commit

--- src/Component/User/User.js
+++ src/Component/User/User.js
@@ -1,32 +1,34 @@
-import React from 'react';
+import React from "react";
 
 const User = (props) => {
-    const familiar = props.familiar
-    let greetings;
-    if (familiar===true){
-        greetings = <p>Welcome My Friend</p>
-    }
-    else{
-        greetings = <p>Who the hell are you ?</p>
-    }
-    //ternary operator
-    const gretting2 = familiar? <h2>kache asho</h2> : <h3>Jaaa Vaaag</h3> 
-    return (
-       <div>
-            <div>
-            {greetings}
-            {gretting2}
-            
-        </div>
-        <div>
-            <h1>Food</h1>
-            {familiar? <h4>yes i will buy food for you </h4>: <h4> no bro his his whos whos</h4>}
-            <br />
-            <h4>condition</h4>
-            {familiar&& <h1>yes i will go with you</h1>}
-        </div>
-       </div>
-    );
+  const familiar = props.familiar;
+  let greetings;
+  if (familiar === true) {
+    greetings = <p>Welcome My Friend</p>;
+  } else {
+    greetings = <p>Who the hell are you ?</p>;
+  }
+  //ternary operator
+  const gretting2 = familiar ? <h2>kache asho</h2> : <h3>Jaaa Vaaag</h3>;
+  return (
+    <div>
+      <div>
+        {greetings}
+        {gretting2}
+      </div>
+      <div>
+        <h1>Food</h1>
+        {familiar ? (
+          <h4>yes i will buy food for you </h4>
+        ) : (
+          <h4> no bro his his whos whos</h4>
+        )}
+        <br />
+        <h4>condition</h4>
+        {familiar && <h1>yes i will go with you</h1>}
+      </div>
+    </div>
+  );
 };
 
 export default User;
